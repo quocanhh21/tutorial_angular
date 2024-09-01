2457e1def8365a8bd7e31a7145df1ffa26594c16
quanh <le.quocanhh21@gmail.com>
2024-09-01 22:52:10 +0700

    Resolved merge conflict in product.component.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,12 @@ import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {
-        path: 'home',
+        path: '',
         component: HomeComponent
+    },
+    {
+        path:'about-us',
+        loadChildren: () =>
+            import('./modules/about-us/about-us.module').then(m => m.AboutUsModule)
     }
 ];
